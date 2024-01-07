94516c9e5de413d48ca47a49812f71063fb60ea1
note9999 <sanghoon273@gmail.com>
2024-01-07 15:57:52 +0900

    절차지향적으로 프로그래밍 - 메서드 추출

--- src/oop1/MusicPlayerMain2.java
+++ src/oop1/MusicPlayerMain2.java
@@ -5,29 +5,49 @@ public class MusicPlayerMain2 {
         MusicPlayerData data = new MusicPlayerData();
 
         // 음악 플레이어 켜기
-        data.isOn = true;
-        System.out.println("음악 플레이어를 시작합니다");
+//        data.isOn = true;
+//        System.out.println("음악 플레이어를 시작합니다");
+        on(data);
 
         // 볼륨 증가
-        data.volume++;
-        System.out.println("음악 플레이어 볼륨:"+data.volume);
+        volumeUp(data);
         // 볼륨 증가
-        data.volume++;
-        System.out.println("음악 플레이어 볼륨:"+data.volume);
+        volumeUp(data);
         // 볼륨 감소
-        data.volume--;
-        System.out.println("음악 플레이어 볼륨:"+data.volume);
+        volumeDown(data);
         // 음악 플레이어 상태
+        showStatus(data);
+
+        // 음악 플레이어 끄기
+        off(data);
+    }
+
+    static void on(MusicPlayerData data) {
+        data.isOn = true;
+        System.out.println("음악 플레이어 시작");
+    }
+
+    static void off(MusicPlayerData data) {
+        data.isOn = false;
+        System.out.println("음악 플레이어 끔");
+    }
+
+    static void volumeUp(MusicPlayerData data) {
+        data.volume++;
+        System.out.println("음악 플레이어 볼륨:" + data.volume);
+    }
+
+    static void volumeDown(MusicPlayerData data) {
+        data.volume--;
+        System.out.println("음악 플레이어 볼륨:" + data.volume);
+    }
+
+    static void showStatus(MusicPlayerData data) {
         System.out.println("음악 플레이어 상태 확인");
-        if(data.isOn){
-            System.out.println("켜져있음,"+"볼륨:"+data.volume);
+        if (data.isOn) {
+            System.out.println("켜져있음," + "볼륨:" + data.volume);
         } else {
             System.out.println("꺼져있음");
         }
-        // 음악 플레이어 끄기
-        data.isOn = false;
-        System.out.println("음악 플레이러를 종료합니다.");
-
-
     }
 }
