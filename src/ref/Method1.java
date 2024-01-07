8a0b726c6d19590ae4deab969a31a20079db5984
note9999 <sanghoon273@gmail.com>
2024-01-07 09:40:03 +0900

    메서드 생성으로 코드 리팩토링

--- src/ref/Method1.java
+++ src/ref/Method1.java
@@ -3,22 +3,22 @@ package ref;
 public class Method1 {
     public static void main(String[] args) {
         Student student1 = new Student();
-        initStudent(student1, name:"학생1", age:15, grade:90);
+        initStudent(student1, "학생1", 15, 90);
 
 //        student1.name = "학생1";
 //        student1.age = 15;
 //        student1.grade = 90;
 
         Student student2 = new Student();
-        initStudent(student2, name);
+        initStudent(student2, "학생2", 16,80);
 //        student2.name = "학생2";
 //        student2.age = 16;
 //        student2.grade = 80;
 
         printStudent((student1));
         printStudent((student2));
-        System.out.println("이름:" + student1.name + "나이:"+student1.age+"성적:"+student1.grade );
-        System.out.println("이름:" + student2.name + "나이:"+student2.age+"성적:"+student2.grade );
+//        System.out.println("이름:" + student1.name + "나이:"+student1.age+"성적:"+student1.grade );
+//        System.out.println("이름:" + student2.name + "나이:"+student2.age+"성적:"+student2.grade );
 
     }
 
---
+++ src/ref/Method2.java
@@ -0,0 +1,50 @@
+package ref;
+
+public class Method2 {
+    public static void main(String[] args) {
+        Student student1 = createStudent("학생1",15,90);
+        Student student2 = createStudent("학생2",16,80);
+
+
+//        Student student1 = new Student();
+//        initStudent(student1, "학생1", 15, 90);
+
+//        student1.name = "학생1";
+//        student1.age = 15;
+//        student1.grade = 90;
+
+//        Student student2 = new Student();
+//        initStudent(student2, "학생2", 16,80);
+//        student2.name = "학생2";
+//        student2.age = 16;
+//        student2.grade = 80;
+
+        printStudent((student1));
+        printStudent((student2));
+//        System.out.println("이름:" + student1.name + "나이:"+student1.age+"성적:"+student1.grade );
+//        System.out.println("이름:" + student2.name + "나이:"+student2.age+"성적:"+student2.grade );
+
+    }
+
+   static Student createStudent(String name, int age , int grade) {
+         Student student = new Student(); // x001
+             student.name = name;
+             student.age = age;
+             student.grade = grade;
+             return student; //x001
+   }
+
+
+//    static void initStudent(Student student, String name, int age, int grade){
+//        student.name = name;
+//        student.age = age;
+//        student.grade = grade;
+//
+//    }
+
+    static void printStudent(Student student){
+        System.out.println("이름:" + student.name + "나이:"+student.age+"성적:"+student.grade );
+
+    }
+
+}
