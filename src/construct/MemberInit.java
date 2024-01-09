d416e082324bf38ec845ac00146f9c4540f68c94
note9999 <sanghoon273@gmail.com>
2024-01-09 16:13:40 +0900

    생성자와 this

---
+++ src/construct/MemberInit.java
@@ -0,0 +1,16 @@
+package construct;
+
+public class MemberInit {
+    String name;
+    int age;
+    int grade;
+
+     //추가
+    void initMember(String name, int age, int grade ){
+      this.name = name;  //this 자기 자신의 인스턴스 가르침
+      this.age = age;
+      this.grade =grade;
+
+    }
+
+}
