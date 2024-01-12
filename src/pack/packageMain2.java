6af3f4073c8aff8582e09a71a9523fbee5779cc6
note9999 <sanghoon273@gmail.com>
2024-01-12 12:45:55 +0900

    패키지 기본 공부

---
+++ src/pack/packageMain2.java
@@ -0,0 +1,9 @@
+package pack;
+
+public class packageMain2 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        pack.a.User user = new pack.a.User();
+
+    }
+}
