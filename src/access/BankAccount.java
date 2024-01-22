05109b73c983c2acd6de46db83909991fc65cb56
note9999 <sanghoon273@gmail.com>
2024-01-22 18:02:02 +0900

    art + insert => generate 단축키

---
+++ src/access/BankAccount.java
@@ -0,0 +1,11 @@
+package access;
+
+public class BankAccount {
+
+    private  int balance;
+
+    // alt + insert => generate 단축키
+    public BankAccount(int balance) {
+        this.balance = balance;
+    }
+}
