4ab8d29da1566e7ad016405f7cf3766297ba6d0c
note9999 <sanghoon273@gmail.com>
2024-02-06 22:28:35 +0900

    이어서 공부

--- src/static1/DataCountMain2.java
+++ src/static1/DataCountMain2.java
@@ -5,8 +5,13 @@ public class DataCountMain2 {
     public static void main(String[] args) {
         Counter counter = new Counter();
         Data2 data1 = new Data2("A",counter);
-        System.out.println("A count"+counter.count);
+        System.out.println("A count="+counter.count);
 
-        Data2 data2 = new Data2(("B", counter));
+        Data2 data2 = new Data2("B", counter);
+        System.out.println("B count="+counter.count);
+
+        Data2 data3 = new Data2("C", counter);
+        System.out.println("C count="+counter.count);
+
     }
 }
